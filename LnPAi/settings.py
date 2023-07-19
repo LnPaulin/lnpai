2516cbf37161146540ea9d1c4a11ca06a98be766
Fonkwa Paulin <fonkwapaulin@gmail.com>
2023-07-19 20:20:47 +0100

    settings static root modifed

--- LnPAi/settings.py
+++ LnPAi/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticiles')
-STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
 LOGIN_REDIRECT_URL = 'dashboard'
